f549d30dbd1c6718a6ba70c4dbb98edb76c7c91b
Coriano Harris <corianoharris@gmail.com>
2021-05-01 14:14:17 -0700

    add color state to top 3 leaders percentages

--- src/Donor.js
+++ src/Donor.js
@@ -2,33 +2,39 @@ import React, {useEffect} from 'react'
 import data from "./data";
 import ReactList from 'react-list';
 
-const Donor =() => {
+const Donor = () => {
 
-  console.log(data[0]);
+  const percentageTop3colors = () => {
+  const percentage = document.querySelectorAll(".percentage");
+  percentage[0].style.color = "#fdd017";
+  percentage[1].style.color = "#b6afa9";
+  percentage[2].style.color = "#b87333";
+  }
+useEffect(()=> {
+  data.sort((a,b) => {
+    return b.current_donation_amount - a.current_donation_amount; 
+  });
+  percentageTop3colors();
+}, []);
 
-  console.log(Object.values(data));
-  
+
 
  const renderItems = () => {
   
+  
   return (
     data.map((donor, index) => (
-      data.sort((a,b) => {
-        return b.current_donation_amount - a.current_donation_amount
-      }),
-      // donationAmount = Math.ceil(Math.abs(Math.random() * donor.donation_goal_amount - donor.current_donation_amount)),
-      // percentage = Math.ceil(Math.abs(donationAmount/donor.donation_goal_amount).toFixed(2) * 100),
       <>
-      <div className="donor__container" key={index}>{donor.sor}
+      <div className="donor__container" key={index}>
         <div className= "donor__profile">
-      <img className="image" src={donor.img} alt="A picture of the donor"/>
+      <img className="image" src={donor.img} alt={"A picture of the donor" + {index}}/>
     <div className="info__container">
       <h2 className="donor__name" >{donor.name}</h2>
       <h4 className="specie__name">{donor.specie}</h4>
     </div>
     </div>
     <div className="donation__container ">
-      <h2 className="percentage">{donor.percentage}</h2>
+      <h2 className="percentage">{Math.floor(Math.abs(donor.current_donation_amount/donor.donation_goal_amount).toFixed(2) * 100)}</h2>
       <div className="amounts" >
         <p className="amounts__text current__amount" >{donor.current_donation_amount}</p>
         <p className="of__text">of</p>
@@ -49,8 +55,8 @@ return (
         itemRenderer={renderItems}
         length={1}
         type="uniform"
-        minSize={data.length}
-        pageSize={data.length}
+        minSize={5}
+        pageSize={6}
       />
     </div>
   </div>
